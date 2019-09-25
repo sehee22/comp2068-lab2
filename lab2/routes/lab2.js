cc3d14d2d5bc6f37b526294d0cad091edae348ae
Sehee Park <200389459@student.georgianc.on.ca>
2019-09-24 21:00:14 -0400

    switch statement instead of if statement

--- lab2/routes/lab2.js
+++ lab2/routes/lab2.js
@@ -4,6 +4,30 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res) {
+    let result;
+
+    switch (req.query.method) {
+        case 'add':
+            result = parseInt(req.query.x) + parseInt(req.query.y);
+            res.send(req.query.x + ' + ' + req.query.y + ' = ' + result);
+            break;
+        case 'subtract':
+            result = parseInt(req.query.x) - parseInt(req.query.y);
+            res.send(req.query.x + ' - ' + req.query.y + ' = ' + result);
+            break;
+        case 'multiply':
+            result = parseInt(req.query.x) * parseInt(req.query.y);
+            res.send(req.query.x + ' * ' + req.query.y + ' = ' + result);
+            break;
+        case 'divide':
+            result = parseInt(req.query.x) / parseInt(req.query.y);
+            res.send(req.query.x + ' / ' + req.query.y + ' = ' + result);
+            break;
+        default:
+            res.send('Input proper method');
+            break;
+    }
+    /*
     if (req.query.method == 'add') {
         let result = parseInt(req.query.x) + parseInt(req.query.y);
         res.send(req.query.x + ' + ' + req.query.y + ' = ' + result)
@@ -23,6 +47,7 @@ router.get('/', function (req, res) {
     else {
         res.send('Input proper method');
     }
+    */
 });
 
 module.exports = router;
