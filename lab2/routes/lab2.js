67f63fb51e3deb77f09d14e67b30db141b628d98
Sehee Park <200389459@student.georgianc.on.ca>
2019-09-24 20:53:32 -0400

    first commit

---
+++ lab2/routes/lab2.js
@@ -0,0 +1,28 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res) {
+    if (req.query.method == 'add') {
+        let result = parseInt(req.query.x) + parseInt(req.query.y);
+        res.send(req.query.x + ' + ' + req.query.y + ' = ' + result)
+    }
+    else if (req.query.method == 'subtract') {
+        let result = parseInt(req.query.x) - parseInt(req.query.y);
+        res.send(req.query.x + ' - ' + req.query.y + ' = ' + result)
+    }
+    else if (req.query.method == 'multiply') {
+        let result = parseInt(req.query.x) * parseInt(req.query.y);
+        res.send(req.query.x + ' * ' + req.query.y + ' = ' + result)
+    }
+    else if (req.query.method == 'divide') {
+        let result = parseInt(req.query.x) / parseInt(req.query.y);
+        res.send(req.query.x + ' / ' + req.query.y + ' = ' + result)
+    }
+    else {
+        res.send('Input proper method');
+    }
+});
+
+module.exports = router;
